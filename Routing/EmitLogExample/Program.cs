9a45f1be660e4df016a9322716efc9109624b664
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-19 15:11:50 +0300

    Add project files.

---
+++ Routing/EmitLogExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new() { HostName = "localhost" };
+IConnection connection = factory.CreateConnection();
+IModel channel  = connection.CreateModel();
+
+
+
+channel.ExchangeDeclare("deneme", type: ExchangeType.Direct);
+
+var message = Console.ReadLine() ?? "boş mesaj";
+if (message == "")
+{
+    message = "boş mesaj";
+}
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange:"deneme",
+    routingKey: "dd",
+    basicProperties: null,
+    body: body);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Routing/ReceiveLogExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new() { HostName = "localhost" };
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare("deneme", type: ExchangeType.Direct);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+//buradaki routing key publish kısımındakinde biraz farklı bunun adına binding key demek karmaşıklığı önleyecektir.
+channel.QueueBind(queue: queueName,
+    exchange: "deneme",
+    routingKey: "dd");
+
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+};
+channel.BasicConsume(queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
