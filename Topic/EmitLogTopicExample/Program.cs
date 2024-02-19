b16f53d95cfa49d0d6ab644e3ccb7f84fa1617ca
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-19 20:08:42 +0300

    End of Day

---
+++ Topic/EmitLogTopicExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new() { HostName = "localhost" };
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare("denemeTopic", ExchangeType.Topic);
+
+var message = Console.ReadLine();
+if (message is null)
+{
+    message = "boş mesaj";
+}
+var body = Encoding.UTF8.GetBytes(message);
+
+var routingKey = args[0];
+
+
+
+channel.BasicPublish("denemeTopic",
+    routingKey: routingKey,
+    basicProperties: null,
+body:body);
+
---
+++ Topic/ReceiveLogsTopicExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new() { HostName = "localhost" };
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare("denemeTopic", ExchangeType.Topic);
+
+
+var queueName = channel.QueueDeclare().QueueName;
+
+foreach (var bindingKey in args)
+{
+    channel.QueueBind(queue: queueName,
+        exchange: "denemeTopic",
+        routingKey: bindingKey);
+}
+
+var consumer =new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+};
+
+
+channel.BasicConsume(queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+
+
+Console.ReadLine();
+
+
+
+
+
+
